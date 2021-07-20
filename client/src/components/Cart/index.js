7f3ccee5a8448008007e965320a014162fb90816
Michael Rodriguez <mikeyrod22@gmail.com>
2021-07-20 16:01:34 -0500

    update cart component

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import CartItem from "../CartItem";
-import { useStoreContext } from "../../utils/GlobalState";
+import store from "../../utils/GlobalState";
+import { useDispatch } from "react-redux";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 import Auth from "../../utils/auth";
@@ -12,14 +13,18 @@ import "./style.css";
 const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = store.useState();
 
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise("cart", "get");
-      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+      dispatch({
+        type: ADD_MULTIPLE_TO_CART,
+        payload: { products: [...cart] },
+      });
     }
 
     if (!state.cart.length) {
